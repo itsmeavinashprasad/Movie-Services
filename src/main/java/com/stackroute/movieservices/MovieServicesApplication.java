251cc2ab2d6d9a1272cf527c6bc0746560c0320b
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-10-10 23:35:36 +0530

    removed extra imports

--- src/main/java/com/stackroute/movieservices/MovieServicesApplication.java
+++ src/main/java/com/stackroute/movieservices/MovieServicesApplication.java
@@ -1,14 +1,7 @@
 package com.stackroute.movieservices;
 
-import com.stackroute.movieservices.domain.Movie;
-import com.stackroute.movieservices.repository.MovieRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.List;
 
 @SpringBootApplication
 public class MovieServicesApplication {
